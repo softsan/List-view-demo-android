98721f5c6714876a5f73c7d477ac3947306b3681
softsan <san.thanki@gmail.com>
2014-09-19 19:26:34 +0530

    List view demo
    sample list view demo

---
+++ EventDemo/DetailView.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Media;
+using Android.Graphics;
+using Android.Provider;
+
+namespace EventDemo
+{
+	[Activity(Label = "Details", Icon = "@drawable/icon")]
+	public class  DetailView : Activity, MediaPlayer.IOnPreparedListener, ISurfaceHolderCallback
+	{
+		MediaPlayer player;
+		VideoView videoView;
+		public static  List<TableItem> TableList { get; set; }
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			try 
+			{
+				base.OnCreate (bundle);
+				
+				// Set our view from the "main" layout resource
+				SetContentView (Resource.Layout.DetailView);
+				
+				string text = Intent.GetStringExtra ("Heading");
+				var table = TableList.FirstOrDefault (c => c.Heading == text);
+				
+				FindViewById<TextView> (Resource.Id.Heading).Text = table.Heading;
+				FindViewById<TextView> (Resource.Id.SubHeading).Text = table.SubHeading;
+				 videoView = FindViewById<VideoView> (Resource.Id.SampleVideoView);
+
+
+
+
+				 play ("TransData/videoviewdemo.mp4");
+			} 
+			catch (Exception ex) 
+			{
+				Console.WriteLine (ex.Message);
+				throw ex;
+			}
+
+//			FindViewById<ImageView>(Resource.Id.branchitem_icon).SetImageURI(Android.Net.Uri.Parse(Branch.ImagePath));
+//
+//			var img = context.Resources.GetIdentifier ("index" + table.ImageResourceId.ToString (), "drawable", context.PackageName);
+//
+//			view.FindViewById<ImageView>(Resource.Id.Image).SetImageResource(img);
+		}
+
+		void play(string fullPath)
+		{
+			ISurfaceHolder holder = videoView.Holder;
+			holder.SetType (SurfaceType.PushBuffers);
+			holder.AddCallback( this );
+			player = new  MediaPlayer ();
+			Android.Content.Res.AssetFileDescriptor afd = this.Assets.OpenFd(fullPath);
+			if  (afd != null )
+			{
+				player.SetDataSource (afd.FileDescriptor, afd.StartOffset, afd.Length);
+				player.Prepare ();
+				player.Start ();
+			}
+		}
+
+		public void SurfaceCreated(ISurfaceHolder holder)
+		{
+			Console.WriteLine("SurfaceCreated");
+			player.SetDisplay(holder);
+		}
+		public void SurfaceDestroyed(ISurfaceHolder holder)
+		{
+			Console.WriteLine("SurfaceDestroyed");
+		}
+		public void SurfaceChanged(ISurfaceHolder holder, Android.Graphics.Format format, int w, int h)
+		{
+			Console.WriteLine("SurfaceChanged");
+		}
+		public void OnPrepared(MediaPlayer player)
+		{
+
+		}
+	}
+}
+
---
+++ EventDemo/MainActivity.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace EventDemo
+{
+    using System.Collections.Generic;
+
+	[Activity(Label = "List",   Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+    {
+		List<TableItem> tableItems;
+		ListView listView;
+        protected override void OnCreate(Bundle bundle)
+        {
+            try
+            {
+                base.OnCreate(bundle);
+
+                //// Set our view from the "main" layout resource
+                // SetContentView(Resource.Layout.Main);
+
+                //items = new string[] { "Kick", "Bang Bang", "Enrique", "Legumes", "US Top 10", "UK Top 10" };
+                //ListAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, items);
+
+				SetContentView(Resource.Layout.Main); // loads the HomeScreen.axml as this activity's view
+				var listView = FindViewById<ListView>(Resource.Id.List); // get reference to the ListView in the layout
+                // populate the listview with data
+				tableItems  = new List<TableItem>();
+
+				for(int i=1;i<=8;i++)
+				{
+					tableItems.Add(new TableItem()
+						{
+//							Heading = "वाइब्रेंट नवरात्री  " + i,
+//							SubHeading = " इस नवरात्री के पावन अवसर पे माँ के अनूप और अलौलिक गरबा का लुफ्त उठाये " + i,
+
+							Heading = "Vibrant Navratri " + i,
+							SubHeading = " Longest dance festival of the universe. Having enchanten colors of clothes, electrifying music makes people very vibrant and enthustics " + i,
+							ImageResourceId = i
+						});
+				}
+
+
+                listView.Adapter = new HomeScreenAdapter(this, tableItems);
+				listView.ItemClick += HandleItemClick;  // to be defined
+            }
+            catch (Exception ex)
+            {
+				Console.WriteLine (ex.Message);
+                throw ex;
+            }
+        }
+
+        void HandleItemClick (object sender, AdapterView.ItemClickEventArgs e)
+        {
+			var listView = sender as ListView;
+			var t = tableItems[e.Position];
+			Android.Widget.Toast.MakeText(this, t.Heading, Android.Widget.ToastLength.Short).Show();
+
+			// Create a branch detail activity
+			var branchDetail = new Intent(this, typeof(DetailView));
+			DetailView.TableList = tableItems;
+
+			// Pass in the town
+			branchDetail.PutExtra("Heading", tableItems[e.Position].Heading);
+
+			// Start the activity
+			StartActivity(branchDetail);
+        }
+
+		 
+
+        private void CreateTab(Type activityType, string tag, string label, int drawableId)
+        {
+            //var intent = new Intent(this, activityType);
+            //intent.AddFlags(ActivityFlags.NewTask);
+
+            //var spec = TabHost.NewTabSpec(tag);
+            //var drawableIcon = Resources.GetDrawable(drawableId);
+            //spec.SetIndicator(label, drawableIcon);
+            //spec.SetContent(intent);
+
+            //TabHost.AddTab(spec);
+        }
+    }
+
+    [Activity]
+    public class SessionsActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            TextView textview = new TextView(this);
+            textview.Text = "This is the session tab";
+            SetContentView(textview);
+        }
+    }
+
+    [Activity]
+    public class MyScheduleActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            TextView textview = new TextView(this);
+            textview.Text = "This is the My Schedule tab";
+            SetContentView(textview);
+        }
+    }
+
+    [Activity]
+    public class SpeakersActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            TextView textview = new TextView(this);
+            textview.Text = "This is the speaker tab";
+            SetContentView(textview);
+        }
+    }
+
+    [Activity]
+    public class WhatsOnActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            TextView textview = new TextView(this);
+            textview.Text = "This is the WhatsOn  tab";
+            SetContentView(textview);
+        }
+    }
+}
+
